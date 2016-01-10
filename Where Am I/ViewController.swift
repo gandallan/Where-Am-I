652b7ded9f4aee4c412a968c697446c4341c4180
Gandhi Mena Salas <gandhimenasalas@macbook-pro-de-gandhi.local>
2016-01-09 20:12:19 -0600

    nuevo commite

--- Where Am I/ViewController.swift
+++ Where Am I/ViewController.swift
@@ -7,18 +7,65 @@
 //
 
 import UIKit
+import CoreLocation
+ 
 
-class ViewController: UIViewController {
 
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    
+    
+//******Outlets
+    
+    @IBOutlet weak var latitudLabel: UILabel!
+    @IBOutlet weak var longitudLabel: UILabel!
+    @IBOutlet weak var courseLabel: UILabel!
+    @IBOutlet weak var speedLabel: UILabel!
+    @IBOutlet weak var altitudLabel: UILabel!
+    @IBOutlet weak var addressLabel: UILabel!
+
+//********Variables
+    
+    var manager: CLLocationManager!
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        manager = CLLocationManager()
+        manager.delegate = self
+        manager.desiredAccuracy = kCLLocationAccuracyBest
+        manager.requestWhenInUseAuthorization()
+        manager.startUpdatingLocation()
+        
+        
     }
+    
+    
+    func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+        print(locations)
+        
+        let userLocation:CLLocation = locations[0]
+        self.latitudLabel.text = "\(userLocation.coordinate.latitude)"
+        self.longitudLabel.text = "\(userLocation.coordinate.longitude)"
+        self.courseLabel.text = "\(userLocation.course)"
+        
+        
+    
+    }
+    
+    
+    
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+        
+
+        
     }
+
 
 
 }
